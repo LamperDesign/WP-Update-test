6e08fd572024ae30f9e20b27a15e1bda901d995b
Kees Lamper <32899122+keeslamper@users.noreply.github.com>
2025-10-22 16:00:17 +0200

    Bump version to 1.2.0 and update URIs

--- wp-update-test.php
+++ wp-update-test.php
@@ -3,12 +3,12 @@
  * Plugin Name: WP Update Test
  * Plugin URI: https://github.com/LamperDesign/WP-Update-Test
  * Description: Test of automatische GitHub updates werken.
- * Version: 1.1.0
+ * Version: 1.2.0
  * Author: Lamper Design
- * GitHub Plugin URI: https://github.com/LamperDesign/WP-Update-Test
+ * GitHub Plugin URI: https://github.com/LamperDesign/wp-update-test
  */
 
 // Gewoon iets simpels:
 add_action('admin_notices', function() {
-    echo '<div class="notice notice-success"><p>WP Update Test plugin is weer actief - versie 1.1.0</p></div>';
+    echo '<div class="notice notice-success"><p>WP Update Test plugin is weer actief - versie 1.2.0</p></div>';
 });
